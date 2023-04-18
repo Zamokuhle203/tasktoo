880d1fecf17a51750c74a4b7d16d9eaa6310c34e
Zamokuhle Nkumane <G20N9632@ict.ru.ac.za>
2023-04-18 13:39:02 +0200

    ADDED TASK 4

--- app/src/main/java/task2/App.java
+++ app/src/main/java/task2/App.java
@@ -1,55 +1,56 @@
 
 package task2;
-import javax.xml.parsers.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.*;
-import java.io.*;
+import javax.xml.parsers.*;
+import org.xml.sax.SAXException;
 
-public class App { 
-    public String getGreeting() {
-        return "Hello World!";
-    }
-    public static void selection(String fieldName) {
+public class XMLToJsonConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the XML file path: ");
+        String filePath = scanner.nextLine();
+        System.out.print("Enter the field name to retrieve: ");
+        String fieldName = scanner.nextLine();
+        scanner.close();
+
         try {
-            // Load the XML file
-            File xmlFile = new File("C:/Users/g20n9632/Documents/java-app/app/src/main/resources/data.xml");
+            // Step 1: Parse the XML file
+            File file = new File(filePath);
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(xmlFile);
+            Document doc = dBuilder.parse(file);
             doc.getDocumentElement().normalize();
 
-            // Get the list of record elements
-            NodeList recordList = doc.getElementsByTagName("record");
-
-            // Loop through each record element
-            for (int i = 0; i < recordList.getLength(); i++) {
-                Node recordNode = recordList.item(i);
-                if (recordNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element recordElement = (Element) recordNode;
-                    System.out.println("Record " + (i + 1) + ":");
-
-                    // Print selected fields
-                    NodeList fieldList = recordElement.getElementsByTagName(fieldName);
-                    if (fieldList.getLength() > 0) {
-                        Element fieldElement = (Element) fieldList.item(0);
-                        System.out.println(fieldName + ": " + fieldElement.getTextContent());
-                    } else {
-                        System.out.println(fieldName + ": Not found");
+            // Step 2: Retrieve the field values and create JSON object
+            NodeList nodeList = doc.getElementsByTagName(fieldName);
+            if (nodeList.getLength() > 0) {
+                JSONArray jsonArray = new JSONArray();
+                for (int i = 0; i < nodeList.getLength(); i++) {
+                    Node node = nodeList.item(i);
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        Element element = (Element) node;
+                        JSONObject jsonObject = new JSONObject();
+                        jsonObject.put(fieldName, element.getTextContent());
+                        jsonArray.put(jsonObject);
                     }
-
-                    System.out.println();
                 }
+                System.out.println(jsonArray.toString());
+            } else {
+                System.out.println("Field not found in the XML file.");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            System.out.println("Error: ParserConfigurationException - " + e.getMessage());
+        } catch (SAXException e) {
+            System.out.println("Error: SAXException - " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error: IOException - " + e.getMessage());
         }
     }
-
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-        //new App().selection();
-        String fieldName = "name";
-        System.out.println("Hello World!");
-        new App().selection(fieldName);
-    }
 }
+
 
